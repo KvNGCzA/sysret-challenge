213319fc417ce6008ade123f7698db259d66c13f
Christopher Akanmu <christopher.akanmu@buzzcast.com>
2022-02-02 16:59:08 +0100

    refactor [rpduct card

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,12 +1,14 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
+type ButtonType = 'regular' | 'stripped' | 'round' | 'display' | 'round secondary';
+
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
-  @Input() type: 'regular' | 'stripped' | 'round' | 'display' | 'round secondary' = 'regular';
+  @Input() type: ButtonType = 'regular';
   @Input() text = 'Enter Button Text';
   @Input() value!: any;
   @Input() disabled = false;
